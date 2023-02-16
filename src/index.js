aa53e28e52352358e5525fc2c9a1bbc483e2a1d8
thang-nv1997 <thangnguyengt12345@gmail.com>
2023-02-16 09:20:02 +0700

    Fetching Movies with a Traditional Promise

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-export function fetchMovies() {
+import { fetchWithTimeout } from "./services";
+const movies = require("./data/movies.json");
 
+export function fetchMovies() {
+    const resolveFunction = () => movies;
+    return fetchWithTimeout(1000).then(resolveFunction);
 }
+
+let moviePromise = fetchMovies();
+moviePromise.then(function(results){console.log(results);})
